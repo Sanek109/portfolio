a896d0fee0da1b592d71326777498dd0bb7ae925
Alex Nabzdorov <Sanek_109@list.ru>
2020-04-15 00:34:38 +0300

    Updates

---
+++ precache-manifest.998df0e5209d0ad07780b6eb148a8515.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af423c16fa8cd58b80e2a0000ab69ee1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "97afc276f21f465e1922",
+    "url": "/portfolio/static/css/main.0195b1d7.chunk.css"
+  },
+  {
+    "revision": "80f6bc250f74eacfcc94",
+    "url": "/portfolio/static/js/2.95440a45.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/portfolio/static/js/2.95440a45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97afc276f21f465e1922",
+    "url": "/portfolio/static/js/main.63e1ac65.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "e266661093a3e9d35ef4a14e4f7357cd",
+    "url": "/portfolio/static/media/icon_css.e2666610.png"
+  },
+  {
+    "revision": "7131844aa5f5fd9f4f8643899a414a95",
+    "url": "/portfolio/static/media/icon_facebook.7131844a.png"
+  },
+  {
+    "revision": "c948a7b1afb411389f7396ce5c9c1898",
+    "url": "/portfolio/static/media/icon_flex.c948a7b1.png"
+  },
+  {
+    "revision": "2a162925c374f3f2222275d6e3e845ce",
+    "url": "/portfolio/static/media/icon_git.2a162925.png"
+  },
+  {
+    "revision": "a3d7942de4961bb5c4ff58dd167b9cc7",
+    "url": "/portfolio/static/media/icon_html.a3d7942d.png"
+  },
+  {
+    "revision": "55fe6e15b620b2743eb0d160f2bf1999",
+    "url": "/portfolio/static/media/icon_instagram.55fe6e15.png"
+  },
+  {
+    "revision": "55db58c8ee718ff3dcc5ba910ca1dd82",
+    "url": "/portfolio/static/media/icon_jquery.55db58c8.png"
+  },
+  {
+    "revision": "22b698a13c9645f1dcad0eeb1a7b859f",
+    "url": "/portfolio/static/media/icon_js.22b698a1.png"
+  },
+  {
+    "revision": "c8a2bd8cfa951023096facff29788c26",
+    "url": "/portfolio/static/media/icon_linkedin.c8a2bd8c.png"
+  },
+  {
+    "revision": "d5e08cd1bc2797eb9f6bcdf90dea6806",
+    "url": "/portfolio/static/media/icon_react.d5e08cd1.png"
+  },
+  {
+    "revision": "ebcfc3d3bf679b0ff995fcf94d9e4cb0",
+    "url": "/portfolio/static/media/icon_redux.ebcfc3d3.png"
+  },
+  {
+    "revision": "33fedf0a3e05fc4374df12bba3d5a3bb",
+    "url": "/portfolio/static/media/icon_sass.33fedf0a.png"
+  },
+  {
+    "revision": "4a2e21f07917f3421054b4fcd71610d9",
+    "url": "/portfolio/static/media/icon_vk.4a2e21f0.png"
+  },
+  {
+    "revision": "eb991de8ba823baa881f5b2f81179032",
+    "url": "/portfolio/static/media/my_photo_2.eb991de8.png"
+  },
+  {
+    "revision": "706bc1ac7a0a28c9f6fb8ae0799d354a",
+    "url": "/portfolio/static/media/social_network.706bc1ac.png"
+  },
+  {
+    "revision": "35513ff513517d89f97472ebf301858a",
+    "url": "/portfolio/static/media/test_site.35513ff5.png"
+  }
+]);
